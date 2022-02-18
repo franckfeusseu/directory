25d706d1d72bb19eb55b663f2fcc7de0ba5c1e03
franck fesse <ffesse@localhost.localdomain>
2022-02-18 23:20:37 +0000

    add settings and requirements directory

--- directory/directory/settings/base.py
+++ directory/directory/settings/base.py
@@ -16,7 +16,6 @@ from telnetlib import AUTHENTICATION
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -28,14 +27,13 @@ DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ['localhost']
 
-
 # Application definition
 
 INSTALLED_APPS = [
     'session_security',
     'django_filters',
     'core',
-    
+
     'django.contrib.postgres',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -84,7 +82,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'directory.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
@@ -98,7 +95,6 @@ DATABASES = {
         'PORT': os.environ.get("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -118,7 +114,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -131,7 +126,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -165,7 +159,7 @@ EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
 
-#django-allauth config
+# django-allauth config
 SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = (
