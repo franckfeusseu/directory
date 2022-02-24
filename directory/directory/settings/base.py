38d813582cb2f75851fa69a4242a40255e3a01d1
franck fesse <ffesse@localhost.localdomain>
2022-02-24 09:24:07 +0000

    sanitize settings

--- directory/directory/settings/base.py
+++ directory/directory/settings/base.py
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'directory.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'directory',
-        'USER': 'directory',
-        'PASSWORD': 'directory',
-        'HOST': 'pweb.cocqv7kcsden.eu-west-2.rds.amazonaws.com',
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
         'PORT': 5432,
     }
 }
